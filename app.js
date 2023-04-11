5421d3e7580ee5df08b85a7bf74ccf8c0ce3794a
Amr Elzahar <amr92621@gmail.com>
2023-04-11 22:02:34 +0200

    root commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
